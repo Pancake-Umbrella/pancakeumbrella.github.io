209540a8a3107e862dae6847fcf849f2915683d2
Carl Blasius <carlblasius@gmail.com>
2024-08-12 23:02:49 +0200

    made carousel and introduced Bootstrap

--- myscripts.js
+++ myscripts.js
@@ -4,6 +4,7 @@ showSlides(slideIndex);
 // Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
+    //showSlides1(n);
 }
 
 function showSlides(n) {
@@ -36,10 +37,39 @@ function showSlidesSmall(n) {
     slides[slideIndex].style.display = "block";
 }
 
-function plusSlides1(n) {
+function showSlides1(n) {
     let i;
     let slides = document.getElementsByClassName("profile");
     let realSlideLength = slides.length -1
+    
+    switch (n){
+        case -1:
+            let idLeft = slides[0].getAttribute('id');
+            console.log(idLeft);
+            document.getElementById(idLeft).style.order += slides.length;
+            console.log(document.getElementById(idLeft).style.order);
+            break;
+        case 1:
+            let idRight = slides[slides.length - 1].getAttribute('id');
+            console.log(idRight);
+            document.getElementById(idRight).style.order -= slides.length;
+            console.log(document.getElementById(idRight).style.order);
+            break;
+        default:
+            console.log("somethings wrong");
+    }
+ /*   
+    if (slideIndex > n) {
+        let id = slides[slides.length].getAttribute('id');
+        console.log(id);
+        id.style.order -= slides.length.toString();
+    } else if (slideIndex < n) {
+        let id = slides[0].getAttribute('id');
+        console.log(id);
+        id.style.order += slides.length.toString();
+    } else { console.log("somethings wrong"); }
+/*/
+    slideIndex = n;
     if (n > realSlideLength) {slideIndex = 0}
     if (n < 0) {slideIndex = realSlideLength}
     for (i = 0; i < slides.length; i++) {
