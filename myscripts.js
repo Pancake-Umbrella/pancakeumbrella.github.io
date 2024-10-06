f14720b858884f3a95cc87e3986c876d3eed3b25
Carl Blasius <carlblasius@gmail.com>
2024-10-06 22:43:20 +0200

    added slider logic for events

--- myscripts.js
+++ myscripts.js
@@ -1,24 +1,47 @@
-let slideIndex = 1;
+let moderatorSlideIndex = 1;
 
 // Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function currentModeratorSlide(n) {
+    showModeratorSlides(moderatorSlideIndex = n);
 }
 
-function showSlides(n) {
+function showModeratorSlides(n) {
     let i;
     let slides = document.getElementsByClassName("Moderator-Slide");
     let dots = document.getElementsByClassName("demo");
     let captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {moderatorSlideIndex = 1}
+    if (n < 1) {moderatorSlideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+    slides[moderatorSlideIndex-1].style.display = "block";
+    dots[moderatorSlideIndex-1].className += " active";
+    captionText.innerHTML = dots[moderatorSlideIndex-1].alt;
+}
+
+
+let eventSlideIndex = 1;
+showEventSlides(eventSlideIndex);
+
+function plusSlides(n) {
+    showEventSlides(eventSlideIndex += n);
+}
+
+function currentSlide(n) {
+    showEventSlides(eventSlideIndex = n);
+}
+
+function showEventSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("Event");
+    if (n > slides.length) {eventSlideIndex = 1}
+    if (n < 1) {eventSlideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[eventSlideIndex-1].style.display = "block";
 }
